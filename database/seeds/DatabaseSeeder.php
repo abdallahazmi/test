552f57dca2bc2b6b5a2be55219e53778897523c4
abdallahazmi <sweup1@gmail.com>
2017-06-14 02:21:58 +0300

    update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,5 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+         factory(App\User::class, 100)->create();
+        factory(App\Category::class, 10)->create();
+        factory(App\Product::class, 100)->create();
     }
 }
